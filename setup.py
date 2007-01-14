165f749452e780b244d778ad567912193aa636c4
micktwomey <micktwomey@182de317-ff26-0410-b031-51b736afe82a>
2007-01-14 12:33:58 +0000

    Adding more package metadata.

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ try:
 except ImportError:
     from distutils import setup
 
-long_description="""Simple utilities for handling ISO 8601 dates
+long_description="""Simple module to parse ISO 8601 dates
+
+This module parses the most common forms of ISO 8601 date strings into
+datetime objects.
 """
 
 setup(
-    name='iso8601',
+    name="iso8601",
     version="0.1",
     description=long_description.split("\n")[0],
     long_description=long_description,
     author="Michael Twomey",
     author_email="micktwomey+iso8601@gmail.com",
-    #url="",
+    url="http://code.google.com/p/pyiso8601/",
     packages=["iso8601"],
 )
